aef101a678d99d0f4c308d1625205f8b06280c70
Valeriya-Kozlova <valeriya410@mail.ru>
2024-12-06 13:32:17 +0300

    change methods request from POST to GET

--- src/main/java/com/example/demo/controller/RespondingController.java
+++ src/main/java/com/example/demo/controller/RespondingController.java
@@ -1,15 +1,12 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class RespondingController {
 
-    @PostMapping("/respond")
-    public String respond(@RequestBody String message) {
+    @GetMapping("/respond")
+    public String respond(@RequestParam String message) {
         return message;
     }
 }
--- src/main/java/com/example/demo/controller/SendingController.java
+++ src/main/java/com/example/demo/controller/SendingController.java
@@ -1,9 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
@@ -17,11 +15,16 @@ public class SendingController {
         this.webClient = webClientBuilder.baseUrl("http://localhost:8080").build();
     }
 
-    @PostMapping("/send")
-    public String sendMessage(@RequestBody String message) {
-        return webClient.post()
-                .uri("/respond")
-                .bodyValue(message)
+    @GetMapping("/send")
+    public String sendMessage(@RequestParam String message) {
+        if (message == null || message.isBlank()) {
+            return "Please, input message!";
+        }
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/respond")
+                        .queryParam("message", message)
+                        .build())
                 .retrieve()
                 .bodyToMono(String.class)
                 .block();
